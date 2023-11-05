e140952173be3f87f2b04af65ab1307bb4066138
ArgusWrites <baylonfrancis4work@gmail.com>
2023-11-05 15:12:18 +0800

    Update: New Error

--- app/src/main/java/com/example/sera_java_ui/MainActivity.java
+++ app/src/main/java/com/example/sera_java_ui/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.sera_java_ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     EditText ed1;
     Button btn;
 
+    private static final int PERMISSION_REQUEST_CODE = 123;
+    private int requestCode;
+    private String[] permissions;
+    private int[] grantResults;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,18 +35,23 @@ public class MainActivity extends AppCompatActivity {
         ed1=(EditText)findViewById(R.id.editText);
         btn=(Button)findViewById(R.id.button);
 
-        tts=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        checkAndRequestPermissions();
+
+        final boolean[] isTtsInitialized = {false};
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status == TextToSpeech.SUCCESS) {
-                    int result = tts.setLanguage(Locale.US);
+                    int result = tts.setLanguage(Locale.getDefault());
                     if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                         Toast.makeText(MainActivity.this, "Language not supported.", Toast.LENGTH_SHORT).show();
                     } else {
-                        // TTS is ready
+                        isTtsInitialized[0] = true;
                     }
                 } else {
                     Toast.makeText(MainActivity.this, "Initialization failed.", Toast.LENGTH_SHORT).show();
+                    Log.d("TTS","Module Status: " + status);
                 }
             }
         });
@@ -44,22 +60,20 @@ public class MainActivity extends AppCompatActivity {
             /** @noinspection deprecation*/
             @Override
             public void onClick(View v) {
-                String toSpeak = ed1.getText().toString().trim();
-                if (!toSpeak.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
-
-                    // Add a delay before speaking
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
-                        }
-                    }, 1000); // Adjust the delay as needed
+                if (isTtsInitialized[0]) {
+                    String toSpeak = ed1.getText().toString().trim();
+                    if (!toSpeak.isEmpty()) {
+                        Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+                        tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Please enter text.", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    Toast.makeText(MainActivity.this, "Please enter text.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "TTS not initialized yet.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
     }
 
     public void onPause(){
@@ -69,4 +83,30 @@ public class MainActivity extends AppCompatActivity {
         }
         super.onPause();
     }
+
+    private void checkAndRequestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.MODIFY_AUDIO_SETTINGS) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.MODIFY_AUDIO_SETTINGS
+            }, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        this.requestCode = requestCode;
+        this.permissions = permissions;
+        this.grantResults = grantResults;
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE && grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission denied. TTS may not work properly.", Toast.LENGTH_SHORT).show();
+            checkAndRequestPermissions();  // Request permissions again
+        }
+    }
+
 }
